a74cad69dca2967fa019826681364318cf88c422
Emile B <emil73d6@edu.eal.dk>
2016-09-06 21:12:33 +0200

    Added Voice struct members

--- update.go
+++ update.go
@@ -103,6 +103,10 @@ type Contact struct {
 }
 
 type Voice struct {
+	FileID   string `json:"file_id"`
+	Duration int    `json:"duration"`
+	MimeType string `json:"mime_type"`
+	FileSize int    `json:"file_size"`
 }
 
 type Venue struct {
