ce2becd9010440faec7dcf8b509e8b011cb12625
Emile B <emil73d6@edu.eal.dk>
2016-09-06 21:23:00 +0200

    Added docs for Update

--- update.go
+++ update.go
@@ -1,12 +1,19 @@
 package TeleGogo
 
+// Update represents a state update in Telegram that has relevance to a bot client.
 type Update struct {
-	Id            int                `json:"update_id"`
-	Message       Message            `json:"message"`
-	EditedMessage Message            `json:"edited_message"`
-	Inline        InlineQuery        `json:"inline_query"`
-	InlineResult  ChosenInlineResult `json:"chosen_inline_result"`
-	Callback      CallbackQuery      `json:"callback_query"`
+	// ID The update‘s unique identifier
+	ID int `json:"update_id"`
+	// Message Optional. New incoming message of any kind — text, photo, sticker, etc.
+	Message Message `json:"message"`
+	// EditedMessage Optional. New version of a message that is known to the bot and was edited
+	EditedMessage Message `json:"edited_message"`
+	// Inline Optional. New incoming inline query
+	Inline InlineQuery `json:"inline_query"`
+	// InlineResult Optional. The result of an inline query that was chosen by a user and sent to their chat partner.
+	InlineResult ChosenInlineResult `json:"chosen_inline_result"`
+	// Callback Optional. New incoming callback query
+	Callback CallbackQuery `json:"callback_query"`
 }
 
 type Message struct {
