c33b50c03260609acdd923d9b064ed6297def6c1
Emile B <emil73d6@edu.eal.dk>
2016-09-06 21:13:53 +0200

    Added Contact struct members

--- update.go
+++ update.go
@@ -100,6 +100,10 @@ type Sticker struct {
 }
 
 type Contact struct {
+	PhoneNumber string `json:"phone_number"`
+	FirstName   string `json:"first_name"`
+	LastName    string `json:"last_name"`
+	UserID      int    `json:"user_id"`
 }
 
 type Voice struct {
