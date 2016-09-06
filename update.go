59b2c392563d3c108a8eeea722f4f05077e1e785
Emile B <emil73d6@edu.eal.dk>
2016-09-06 21:04:17 +0200

    Added Audio struct members

--- update.go
+++ update.go
@@ -67,6 +67,12 @@ type MessageEntity struct {
 }
 
 type Audio struct {
+	FileID    string `json:"file_id"`
+	Duration  int    `json:"duration"`
+	Performer string `json:"performer"`
+	Title     string `json:"title"`
+	MimeType  string `json:"mime_type"`
+	FileSize  int    `json:"file_size"`
 }
 
 type Document struct {
