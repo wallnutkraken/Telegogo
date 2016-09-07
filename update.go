3f87671fc6b7c2c50773fd498d8f3a7bd1fd7320
Emile B <emil73d6@edu.eal.dk>
2016-09-07 09:03:08 +0200

    Filled in InlineQuery

--- update.go
+++ update.go
@@ -84,7 +84,19 @@ type Message struct {
 	PinnedMessage *Message `json:"pinned_message"`
 }
 
+// InlineQuery represents an incoming inline query. When the user sends an empty query,
+// your bot could return some default or trending results.
 type InlineQuery struct {
+	// ID Unique identifier for this query
+	ID string `json:"id"`
+	// From Sender
+	From User `json:"from"`
+	// Location Optional. Sender location, only for bots that request user location
+	Location Location `json:"location"`
+	// Query Text of the query (up to 512 characters)
+	Query string `json:"query"`
+	// Offset Offset of the results to be returned, can be controlled by the bot
+	Offset string `json:"offset"`
 }
 
 type ChosenInlineResult struct {
