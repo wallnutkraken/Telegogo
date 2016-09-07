ea15664f1de90cd254236fcb422d2017ea58777f
Emile B <emil73d6@edu.eal.dk>
2016-09-07 09:24:31 +0200

    Filled in chat struct

--- update.go
+++ update.go
@@ -140,7 +140,20 @@ type User struct {
 	Username string `json:"username"`
 }
 
+// Chat represents a chat.
 type Chat struct {
+	// ID Unique identifier for this chat.
+	ID int `json:"id"`
+	// Type of chat, can be either “private”, “group”, “supergroup” or “channel”
+	Type string `json:"type"`
+	// Title Optional. Title, for channels and group chats
+	Title string `json:"title"`
+	// Username Optional. Username, for private chats, supergroups and channels if available
+	Username string `json:"username"`
+	// FirstName Optional. First name of the other party in a private chat
+	FirstName string `json:"first_name"`
+	// LastName Optional. Last name of the other party in a private chat
+	LastName string `json:"last_name"`
 }
 
 // MessageEntity represents one special entity in a text message. For example, hashtags, usernames, URLs, etc.
