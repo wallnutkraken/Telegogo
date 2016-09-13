6880cd595d1efeaef3c2d99df31f634a084f451a
Emile B <emil73d6@edu.eal.dk>
2016-09-13 18:57:40 +0200

    changed location values from float64 to float32

--- location.go
+++ location.go
@@ -3,7 +3,7 @@ package TeleGogo
 // Location represents a point on the map.
 type Location struct {
 	// Longitude as defined by sender
-	Longitude float64 `json:"longitude"`
+	Longitude float32 `json:"longitude"`
 	// Latitude as defined by sender
-	Latitude float64 `json:"latitude"`
+	Latitude float32 `json:"latitude"`
 }
